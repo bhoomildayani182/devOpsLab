17d7012126278d46fbdc22d4d6738a2f2bcd3326
bhoomildayani182 <bhoomildayani182@gmail.com>
2023-03-14 13:37:46 +0530

    update

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, World and I am BhoomilDayani";
+        return "Hello, World and I am Bhoomil Dayani";
     }
 
 }
